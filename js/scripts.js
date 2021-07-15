dc5b0c0a0549c048cece4b9306b4048712593045
rohusemer <ronhuseg@espol.edu.ec>
2021-07-15 02:33:39 -0500

    se agrego un JQuery en el archivo js, ademas de incluir um modal para el login en cada pantalla

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
-    $("#btLogin").click(function(){
+    $("#botonLogin").click(function(){
         $('#loginModal').modal('toggle');
     });
+ 
+    $("#SigL").click(function(){
+      $('#loginModal').modal('toggle');
+      $("#btI").html(`<a id="newUser"  href="index.html">usuario@correo.ec <span id="salir" type="submit" class="fa fa-sign-in"></span> </a>`);
+    });  
+
+    $("#btnLogin").click(function(){
+      $('#regisModal').modal('toggle');
+    });
+
 });
 
 /*function cambiarPag() {
