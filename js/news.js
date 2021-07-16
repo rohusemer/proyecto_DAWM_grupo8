cab971b304e2bea03792290a23f91edcda291d3b
Cesar Carlier <ccarlier@espol.edu.ec>
2021-07-16 09:01:18 -0700

    Dashboard y noticias

---
+++ js/news.js
@@ -0,0 +1,22 @@
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI('0ea8b3a45c434748b0d10ed194209f28');
+// To query /v2/top-headlines
+// All options passed to topHeadlines are optional, but you need to include at least one of them
+newsapi.v2.topHeadlines({
+  sources: 'bbc-news,the-verge',
+  q: 'bitcoin',
+  category: 'business',
+  language: 'en',
+  country: 'us'
+}).then(response => {
+  console.log(response);
+  /*
+    {
+      status: "ok",
+      articles: [...]
+    }
+  */
+});
+
+
+<script src="js/news.js"></script>
